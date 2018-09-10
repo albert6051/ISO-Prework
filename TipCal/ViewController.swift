45f755f0f78349221410d924f8fcd56308358b75
Gong Qi Chen <albert6051@gongs-imac.local>
2018-09-09 17:11:13 -0700

    Initial Commit

---
+++ TipCal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCal
+//
+//  Created by Gong Qi Chen on 9/9/18.
+//  Copyright © 2018 Gong Qi Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
